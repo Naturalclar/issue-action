67c40172a1f6c712dabcabdfc58d6eb7037e50e2
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-10-13 01:34:39 +0900

    fix: getIssueContent test

--- __tests__/getIssueContent.test.ts
+++ __tests__/getIssueContent.test.ts
@@ -2,5 +2,5 @@ import { getIssueContent } from "../src/getIssueContent";
 
 test("throws error if no issue provided", async () => {
   const input = "foo";
-  await expect(getIssueContent(input)).rejects.toThrow("No Issue Provided");
+  await expect(getIssueContent(input)).rejects.toThrow("Bad credentials");
 });
