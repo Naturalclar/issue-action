5d33a0f941df8f8c1b4212edbf0c88e75bf7419a
Somaya Bounouar <somayb@amazon.com>
2020-06-01 13:13:45 -0700

    update tests

--- __tests__/getIssueContent.test.ts
+++ __tests__/getIssueContent.test.ts
@@ -2,5 +2,6 @@ import { getIssueContent } from "../src/getIssueContent";
 
 test("throws error if no issue provided", async () => {
   const input = "foo";
-  await expect(getIssueContent(input)).rejects.toThrow("No Issue Provided");
+  const titleOrBody = "both";
+  await expect(getIssueContent(input, titleOrBody)).rejects.toThrow("No Issue Provided");
 });
