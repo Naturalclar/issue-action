1f88d5ee42e07e8540a773f2f303c1a26d6a2f75
Somaya Bounouar <somayb@amazon.com>
2020-06-01 17:00:17 -0700

    fix test-2

--- __tests__/checkKeywords.test.ts
+++ __tests__/checkKeywords.test.ts
@@ -1,13 +1,13 @@
 import { checkKeywords } from "../src/checkKeywords";
 
-describe("checkKeyword", () => {
+describe("checkKeywords", () => {
   it("returns an array of matching keyword objects if keyword is included in the issue content", () => {
     const result = checkKeywords([{"keywords": ["test"], "labels": ["test"], "assignees": ["Naturalclar"]}], "test");
-    expect(result).toEqual([{"keywords": ["test", "bar"], "labels": ["test"], "assignees": ["Naturalclar"]}]);
+    expect(result).toEqual([{"keywords": ["test"], "labels": ["test"], "assignees": ["Naturalclar"]}]);
   });
   it("returns an array of matching keyword objects even if different casings in keyword", () => {
     const result = checkKeywords([{"keywords": ["test", "Bar"], "labels": ["test"], "assignees": ["Naturalclar"]}], "bar");
-    expect(result).toEqual([{"keywords": ["test", "bar"], "labels": ["test"], "assignees": ["Naturalclar"]}]);
+    expect(result).toEqual([{"keywords": ["test", "Bar"], "labels": ["test"], "assignees": ["Naturalclar"]}]);
   });
   it("returns an array of matching keyword objects even if different casings in issue content", () => {
     const result = checkKeywords([{"keywords": ["test", "bar"], "labels": ["test"], "assignees": ["Naturalclar"]}], "Bar");
