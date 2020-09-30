6bb56ff9125f54f867dd52c1ceeaaf3bfb03bc6a
Jesse Katsumata <jesse.katsumata@gmail.com>
2020-09-30 11:14:53 +0900

    fix test

--- __tests__/getIssueContent.test.ts
+++ __tests__/getIssueContent.test.ts
@@ -1,9 +1,9 @@
 import { getIssueContent } from "../src/getIssueContent";
 
 test("throws error if no issue provided", async () => {
-  const input = process.env.GITHUB_TOKEN || "foo";
+  const token = process.env.GITHUB_TOKEN || "foo";
   const titleOrBody = "both";
-  await expect(getIssueContent(input, titleOrBody)).rejects.toThrow(
+  await expect(getIssueContent(token, titleOrBody)).rejects.toThrow(
     "No Issue Provided"
   );
 });
