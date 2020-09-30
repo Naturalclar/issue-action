29a6a6d94d240932ce8496ae20ced27227425af2
Jesse Katsumata <niconico.clarinet@gmail.com>
2020-09-30 11:40:09 +0900

    chore: use vercel ncc (#132)

--- __tests__/checkKeywords.test.ts
+++ __tests__/checkKeywords.test.ts
@@ -2,20 +2,78 @@ import { checkKeywords } from "../src/checkKeywords";
 
 describe("checkKeywords", () => {
   it("returns an array of matching keyword objects if keyword is included in the issue content", () => {
-    const result = checkKeywords([{"keywords": ["test"], "labels": ["test"], "assignees": ["Naturalclar"]}], "test");
-    expect(result).toEqual([{"keywords": ["test"], "labels": ["test"], "assignees": ["Naturalclar"]}]);
+    const result = checkKeywords(
+      [{ keywords: ["test"], labels: ["test"], assignees: ["Naturalclar"] }],
+      "test"
+    );
+    expect(result).toEqual([
+      { keywords: ["test"], labels: ["test"], assignees: ["Naturalclar"] },
+    ]);
   });
   it("returns an array of matching keyword objects even if different casings in keyword", () => {
-    const result = checkKeywords([{"keywords": ["test", "Bar"], "labels": ["test"], "assignees": ["Naturalclar"]}], "bar");
-    expect(result).toEqual([{"keywords": ["test", "Bar"], "labels": ["test"], "assignees": ["Naturalclar"]}]);
+    const result = checkKeywords(
+      [
+        {
+          keywords: ["test", "Bar"],
+          labels: ["test"],
+          assignees: ["Naturalclar"],
+        },
+      ],
+      "bar"
+    );
+    expect(result).toEqual([
+      {
+        keywords: ["test", "Bar"],
+        labels: ["test"],
+        assignees: ["Naturalclar"],
+      },
+    ]);
   });
   it("returns an array of matching keyword objects even if different casings in issue content", () => {
-    const result = checkKeywords([{"keywords": ["test", "bar"], "labels": ["test"], "assignees": ["Naturalclar"]}], "Bar");
-    expect(result).toEqual([{"keywords": ["test", "bar"], "labels": ["test"], "assignees": ["Naturalclar"]}]);
+    const result = checkKeywords(
+      [
+        {
+          keywords: ["test", "bar"],
+          labels: ["test"],
+          assignees: ["Naturalclar"],
+        },
+      ],
+      "Bar"
+    );
+    expect(result).toEqual([
+      {
+        keywords: ["test", "bar"],
+        labels: ["test"],
+        assignees: ["Naturalclar"],
+      },
+    ]);
   });
 
   it("returns null if keyword is not included in issue content", () => {
-    const result = checkKeywords([{"keywords": ["test"], "labels": ["test"], "assignees": ["Naturalclar"]}], "");
+    const result = checkKeywords(
+      [{ keywords: ["test"], labels: ["test"], assignees: ["Naturalclar"] }],
+      ""
+    );
     expect(result).toBeNull();
+  });
+  it("returns null if keyword is surrounded by character", () => {
+    const result = checkKeywords(
+      [{ keywords: ["test"], labels: ["test"], assignees: ["Naturalclar"] }],
+      "testable"
+    );
+    expect(result).toBeNull();
+  });
+  it("matches if keyword is surrounded by 2 byte space", () => {
+    const result = checkKeywords(
+      [{ keywords: ["test"], labels: ["test"], assignees: ["Naturalclar"] }],
+      "　test　" // 2byte space for Japanese or Chinese characters
+    );
+    expect(result).toEqual([
+      {
+        keywords: ["test"],
+        labels: ["test"],
+        assignees: ["Naturalclar"],
+      },
+    ]);
   });
 });
--- __tests__/getIssueContent.test.ts
+++
@@ -1,7 +0,0 @@
-import { getIssueContent } from "../src/getIssueContent";
-
-test("throws error if no issue provided", async () => {
-  const input = "foo";
-  const titleOrBody = "both";
-  await expect(getIssueContent(input, titleOrBody)).rejects.toThrow("No Issue Provided");
-});
