0077f34c57f91c34789dd303f59aa341644cb4e8
peterwoodworth <woodwoop@amazon.com>
2021-03-30 14:37:45 -0700

    orderbranch

--- src/countKeywords.ts
+++ src/countKeywords.ts
@@ -5,9 +5,8 @@ export const countKeywords = (
 
   let issueWords: string[] = content.split(" ");
   let areaMap = new Map();
-  // push used Keywords into array to devalue them if keyword shows up again
-  let usedKeywords: string[] = [];
-  const DEVALUE: number = .75;
+  let devalue: number = 1;
+  let devalueModifier: number = 2/issueWords.length
   let newValue: number;
 
   // Count keywords in each area by looking at each word in content and counting it to an area if it is a keyword of that area
@@ -15,24 +14,17 @@ export const countKeywords = (
     parameters.forEach(obj => {
       obj.keywords.forEach(keyword => {
         // TODO adjust (word === keyword) to be less picky (similar word library, regex, toLower, keyword in word)
-        if(word === keyword && usedKeywords.includes(keyword)) {
+        if(word === keyword) {
           if(areaMap.has(obj.area)) {
-            newValue = areaMap.get(obj.area)+DEVALUE
+            newValue = areaMap.get(obj.area)+devalue
             areaMap.set(obj.area, newValue);
           } else {
               areaMap.set(obj.area, 1);
-            }
-        } else if(word === keyword) {
-          usedKeywords.push(word)
-          if(areaMap.has(obj.area)) {
-            newValue = areaMap.get(obj.area)+1
-            areaMap.set(obj.area, newValue);
-          } else {
-            areaMap.set(obj.area, 1);
           }
         }
       })
     })
+    devalue -= devalueModifier;
   })
 
   // Determine which area has the most matches
