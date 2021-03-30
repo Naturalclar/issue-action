57c28682e4cedbea91ab91d44ca163eaab48f911
peterwoodworth <woodwoop@amazon.com>
2021-03-30 12:43:30 -0700

    new branch

--- src/countKeywords.ts
+++ src/countKeywords.ts
@@ -3,17 +3,22 @@ export const countKeywords = (
   content: string
 ): string => {
 
-  let issueWords = content.split(" ");
-  let areaMap = new Map()
+  let issueWords: string[] = content.split(" ");
+  let areaMap = new Map();
+  let usedKeywords: string[] = [];
+  const DEVALUE: number = .75;
 
   // Count keywords in each area by looking at each word in content and counting it to an area if it is a keyword of that area
   issueWords.forEach(word => {
     parameters.forEach(obj => {
       obj.keywords.forEach(keyword => {
-          // TODO adjust (word === keyword) to be less picky (similar word library, regex, toLower)
-        if(word === keyword) {
-          areaMap.has(obj.area) ? areaMap[obj.area]++ : areaMap.set(obj.area, 1)  
-        }    
+        // TODO adjust (word === keyword) to be less picky (similar word library, regex, toLower, keyword in word)
+        if(word === keyword && usedKeywords.includes(keyword)) {
+          areaMap.has(obj.area) ? areaMap[obj.area]+=DEVALUE : areaMap.set(obj.area, 1);
+        } else if(word === keyword) {
+          areaMap.has(obj.area) ? areaMap[obj.area]++ : areaMap.set(obj.area, 1);
+          usedKeywords.push(word);
+        }
       })
     })
   })
@@ -22,9 +27,10 @@ export const countKeywords = (
   let winningArea = '';
   let max = 0;
   for (let area of areaMap.entries()) {
+    console.log(area)
     if (area[1] > max) {
-      winningArea = area[0]
-      max = area[1]
+      winningArea = area[0];
+      max = area[1];
     }
   }
 
