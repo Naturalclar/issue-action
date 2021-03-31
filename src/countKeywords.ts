2a04dd69de426be898bb0942742c49ba743b63c4
peterwoodworth <woodwoop@amazon.com>
2021-03-31 12:03:29 -0700

    fixes

--- src/countKeywords.ts
+++ src/countKeywords.ts
@@ -1,41 +1,28 @@
+import { scoreArea } from './scoreArea'
+
 export const countKeywords = (
   parameters: { area: string, keywords: string[], labels: string[], assignees: string[] }[],
   content: string
 ): string => {
 
-  let issueWords: string[] = content.split(" ");
-  let areaMap = new Map();
-  // Linearly lower value of each word starting at 1 and ending at .5
-  let devalue: number = 1;
-  let devalueModifier: number = .5/issueWords.length;
-  let newValue: number;
+  let potentialAreas: Map<string, number> = new Map();
+  let issueWords = content.split(" ");
+  let devalue: number = 1, devalueCounter: number = 1;
 
   // Count keywords in each area by looking at each word in content and counting it to an area if it is a keyword of that area
-  issueWords.forEach(word => {
-    parameters.forEach(obj => {
-      obj.keywords.forEach(keyword => {
-        // TODO adjust (word === keyword) to be less picky (similar word library, regex, toLower, keyword in word)
-        if(word === keyword) {
-          if(areaMap.has(obj.area)) {
-            newValue = areaMap.get(obj.area)+devalue
-            areaMap.set(obj.area, newValue);
-          } else {
-              areaMap.set(obj.area, 1);
-          }
-        }
-      })
-    })
-    devalue -= devalueModifier;
-  });
+  issueWords.forEach(content => {
+    potentialAreas = scoreArea(content, parameters, potentialAreas, devalue);
+    devalue = 1/++devalueCounter;
+  })
 
   // Determine which area has the most matches
   let winningArea = '';
   let max = 0;
-  for (let area of areaMap.entries()) {
-    console.log(area);
+  for (let area of potentialAreas.entries()) {
+    console.log(area)
     if (area[1] > max) {
-      winningArea = area[0];
-      max = area[1];
+      winningArea = area[0]
+      max = area[1]
     }
   }
 
---
+++ src/scoreArea.ts
@@ -0,0 +1,22 @@
+export const scoreArea = (
+    content: string,
+    parameters: { area: string, keywords: string[], labels: string[], assignees: string[] }[],
+    potentialAreas,
+    devalue
+): Map<string, number> => {
+  
+  // Count keywords in each area by looking at each word in content and counting it to an area if it is a keyword of that area
+  parameters.forEach(obj => {
+    obj.keywords.forEach(keyword => {
+      // TODO adjust (word === keyword) to be less picky (similar word library, regex, toLower)
+      if(content === keyword) {
+        potentialAreas.has(obj.area) ?
+          potentialAreas.set(obj.area, potentialAreas.get(obj.area)+devalue) :
+          potentialAreas.set(obj.area, devalue);
+        return potentialAreas;
+      }    
+    })
+  })
+
+  return potentialAreas;
+}
