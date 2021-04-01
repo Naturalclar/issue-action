182b4c60d88b3aa0229bdb70a9471d122ff4913a
peterwoodworth <woodwoop@amazon.com>
2021-04-01 13:48:21 -0700

    logging branch

--- src/countKeywords.ts
+++ src/countKeywords.ts
@@ -5,17 +5,17 @@ export const countKeywords = (
   content: string
 ): string => {
 
-  let potentialAreas: Map<string, number> = new Map();
   let issueWords = content.split(/ |\./);
   let devalue: number = 1, devalueCounter: number = 1;
-  let detectedKeywords: string[] = [];
   let returnObject: { potentialAreasMap: Map<string, number>, detectedKeywords: string[]} = { potentialAreasMap: new Map(), detectedKeywords: []}
 
   // Count keywords in each area by looking at each word in content and counting it to an area if it is a keyword of that area
   issueWords.forEach(content => {
-    returnObject = scoreArea(content, parameters, potentialAreas, devalue, detectedKeywords);
+    returnObject = scoreArea(content, parameters, returnObject.potentialAreasMap, devalue, returnObject.potentialAreasMap);
     devalue = 1/++devalueCounter;
   })
+
+  console.log(returnObject.detectedKeywords);
 
   // Determine which area has the most matches
   let winningArea = '';
