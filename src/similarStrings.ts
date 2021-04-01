55a95c2d03d49c423b020ce88b14f8a5bfd45012
peterwoodworth <woodwoop@amazon.com>
2021-04-01 16:00:59 -0700

    logging branch

--- src/similarStrings.ts
+++ src/similarStrings.ts
@@ -3,10 +3,10 @@ import levenshtein from 'js-levenshtein';
 export const similarStrings = (str1: string, str2: string): boolean => {
     str1 = str1.toLowerCase();
     str2 = str2.toLowerCase();
-    str1.replace('/ |_|-|(|)|	|\./gi', '');
-    str2.replace('/ |_|-|(|)|	|\./gi', '');
+    str1.replace(/ |_|-|(|)|	|\./gi, '');
+    str2.replace(/ |_|-|(|)|	|\./gi, '');
   ​
-    if(levenshtein(str1, str2) <= (str1.length + str2.length / 2) * .125) {
+    if(levenshtein(str1, str2) <= Math.ceil((str1.length + str2.length / 2) * .15)) {
       console.log(str1, str2)
       return true;
     }
