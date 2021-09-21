796362225a20d25018cc961c0e87f6ed28960aa9
Tom Keller <1083460+kellertk@users.noreply.github.com>
2021-09-21 16:49:50 -0700

    Add catch handling to async run() (#6)
    Should catch errors and terminate action run when failing.

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,25 @@ import { IParameter, Issue } from './issue';
 import { GithubApi } from './github';
 
 async function run() {
-  try {
-    core.setOutput("labeled", false.toString());
-    core.setOutput("assigned", false.toString());
+  core.setOutput("labeled", false.toString());
+  core.setOutput("assigned", false.toString());
 
-    const token = core.getInput('github-token');
-    const github: GithubApi = new GithubApi(token);
-    const content: string[] = await github.getIssueContent();
-    const issue: Issue = new Issue(content);
-    const winningAreaData: IParameter = issue.getWinningAreaData(issue.determineArea())
+  const token = core.getInput('github-token');
+  const github: GithubApi = new GithubApi(token);
+  const content: string[] = await github.getIssueContent();
+  const issue: Issue = new Issue(content);
+  const winningAreaData: IParameter = issue.getWinningAreaData(issue.determineArea())
 
-    if (winningAreaData.area === '') { 
-      console.log("Keywords not included in this issue");
-    } else {
-      github.setIssueAssignees(winningAreaData.assignees);
-      github.setIssueLabels(winningAreaData.labels);
-      core.setOutput("labeled", true.toString());
-      core.setOutput("assigned", true.toString());
-    }
-  } catch (error) {
-    core.setFailed(error.message);
+  if (winningAreaData.area === '') { 
+    console.log("Keywords not included in this issue");
+  } else {
+    github.setIssueAssignees(winningAreaData.assignees);
+    github.setIssueLabels(winningAreaData.labels);
+    core.setOutput("labeled", true.toString());
+    core.setOutput("assigned", true.toString());
   }
 }
 
-run();
+run().catch((error) => {
+  core.setFailed(error.message);
+});
